033ba93b052520b8b173efc525ed981c812a0e0f
David Melvin <davidmelvinjr@gmail.com>
2015-05-25 05:02:05 +0000

    basically done

--- pi.py
+++ pi.py
@@ -23,24 +23,24 @@ canvas = tk.Canvas(root, width=790, height=480, bg="#3b5998")
 canvas.pack()
 camera = picamera.PiCamera()
 
-def stroke_text(x, y, text, size, textcolor, strokecolor):
+def stroke_text(x, y, text, size, textcolor, strokecolor, anchoring):
     # make stroke text
-    canvas.create_text(x, y, text=text, font=('Arial', size, 'bold'), fill=strokecolor)
+    canvas.create_text(x+1, y+1, text=text, font=('Arial', size), fill=strokecolor, anchor=anchoring)
     # make regular text
-    canvas.create_text(x, y, text=text, font=('Arial', size), fill=textcolor)
+    canvas.create_text(x, y, text=text, font=('Arial', size), fill=textcolor, anchor=anchoring)
 
 
 def showInstructions():
-  stroke_text(350, 30, "Welcome to FaceBox!",  20, "white", "black")
-  canvas.create_text(15, 70, text="Instructions:", font="Arial 18", fill="#8b9dc3", anchor="w")
+  stroke_text(320, 40, "Welcome to FaceBox!",  22, "white", "black", "center")
+  canvas.create_text(15, 100, text="Instructions:", font="Arial 18", fill="#8b9dc3", anchor="w")
 
-  canvas.create_text(35, 110, text = "1. Press the red shutter button.\n. Your picture will be picture will be taken after 3 seconds.'\n3. After the picture is taken you will be able to view it.\n4. Press the confirm button to upload the image to Facebook.\n5. Press nothing for 10 seconds to cancel the upload.",  font="Arial 16", fill="#dfe3ee", anchor="w")
+  canvas.create_text(35, 150, text = "1. Press the red shutter button.\n2. Your picture will be picture will be taken after 3 seconds.\n3. After the picture is taken you will be able to view it.\n4. Press the confirm button to upload the image to Facebook.\n5. Press the red button or nothing for 10 seconds to cancel\nthe upload.",  font="Arial 16", fill="#dfe3ee", anchor="nw")
   #canvas.create_text(35, 110, text="1. Press the red shutter button.", fill="#dfe3ee", font="Arial 16", anchor="w")
   #canvas.create_text(35, 150, text="2. Your picture will be picture will be taken after 10 seconds.", font="Arial 16", fil="#dfe3ee", anchor="w")
   #canvas.create_text(35, 190, text="3. After the picture is taken you will be able to view it.", font="Arial 16", fill="#dfe3ee", anchor="w")
   #canvas.create_text(35, 230, text="4. Press the confirm button to upload the image to Facebook.", font="Arial 16", fill="#dfe3ee", anchor="w")
 
-  stroke_text(350, 350, "Press the RED shutter button now to take a picture!", 20, "white", "black")
+  stroke_text(15, 400, "Press the RED shutter button now to take a picture!", 20, "white", "black", "w")
 
 def checkButton():
   showInstructions()
@@ -49,21 +49,26 @@ def checkButton():
     shutter_input = GPIO.input(17)
     if (shutter_input==1):
       canvas.delete("all")
+      stroke_text(340, 10, "Taking picture in 5 seconds...", 12, "white", "black", "center")
+      canvas.update()
       GPIO.output(22,False) #Turn Shutter Button LED off during upload/confirm phase
       print "Shutter button pressed, taking photo in three seconds"
       
-      canvas.create_text(375, 10, text="Here is a preview of your picture:", font="Arial 12")
       camera.resolution = (750,450)
       camera.start_preview()
       time.sleep(5)
       camera.stop_preview()
       camera.capture('FaceBox_Pic.jpg')
+      canvas.delete("all")
+      stroke_text(340, 240, "Loading Picture...", 24, "white", "black", "center")
+      canvas.update()
       
       break
-
+  
+  
   print "Waiting for upload button press"
   print "displaying photo with Tkinter"
-
+  
   imageFile = "FaceBox_Pic.jpg"
   image1 = ImageTk.PhotoImage(Image.open(imageFile))
   w = image1.width()
@@ -71,15 +76,16 @@ def checkButton():
    
   canvas.create_image(0,20, anchor="nw", image=image1)
 
-  canvas.create_text(375, 10, text="Press the blue button to post this picture:", font="Arial 12", fill = "white")
+  stroke_text(350, 10, "Press the blue button to post or red button to cancel:", 12, "white", "black", "center")
 
   canvas.update()
   GPIO.output(23,True)
   
   t1 = datetime.now()
   while True:
-    input = GPIO.input(18)
-    if (input==1):
+    uploadButton = GPIO.input(18)
+    cancelButton = GPIO.input(17)
+    if (uploadButton==1):
      print "Uploading picture.."
      graph.post(
        path = '1583747018566887/photos',
@@ -90,7 +96,7 @@ def checkButton():
      
     t2 = datetime.now()
     delta = t2 - t1
-    if (delta.seconds > 10):
+    if (delta.seconds > 10 or cancelButton ==1):
       print "Upload not pressed within 10 seconds.. breaking now."
       break
   
